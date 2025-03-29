1669606e9ec8807f0e554c3219b10694737fdafd
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-03-29 13:00:28 +0530

    Add files via upload

---
+++ Additionsum.java
@@ -0,0 +1,13 @@
+package com.Lambda_Expression;
+@FunctionalInterface
+public interface Additionsum
+{
+void add(int a,int b);
+}
+
+//==================Method 2================//
+//@FunctionalInterface
+//public interface Addtitionsum
+//{
+//void add(int a,int b);
+//}
